dd842911facd4ce6479c45c1ce57116e4f4eeb14
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-03 14:04:48 -0600

    add iteration over fakedata to render different information on Card component.

--- front-end/src/app/dashboard/page.js
+++ front-end/src/app/dashboard/page.js
@@ -1,13 +1,42 @@
 import Card from "@/components/card";
 
+const example=[
+  {
+    title:"My Very Long Book Title To See If it Fits and to Optimize for Large Titles.",
+    author:"Robert Matthew Rueter von Helsing Schweitzerdreng",
+    language:"Spanish",
+    status:"not read"
+  },
+  {
+    title:"My Somewhat Long Book Title.",
+    author:"Robert Matthew Rueter von Helsing",
+    language:"Spanish",
+    status:"read"
+  },
+  {
+    title:"My Short Title",
+    author:"Robert Matthew Rueter",
+    language:"Spanish",
+    status:"read"
+  },
+]
+
+
 function Dashboard (){
     return(
         <>
-          <h1>I'm the Dashboard</h1>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <section>
+            <p>Sort</p>
+            <button className="border-2 border-black rounded-md bg-red-300 p-2 m-1">Title</button>
+            <button className="border-2 border-black rounded-md bg-red-300 p-2 m-1">Author</button>
+            <button className="border-2 border-black rounded-md bg-red-300 p-2 m-1">Status</button>
+            <button className="border-2 border-black rounded-md bg-red-300 p-2 m-1">Language</button>
+          </section>
+          <ul>
+            {example.map((book, index ) =>(
+              <Card key={index} bookInfo={book} />
+            ))}
+          </ul>
         </>
     )
 }
--- front-end/src/components/card.js
+++ front-end/src/components/card.js
@@ -1,30 +1,26 @@
-const example={
-    title:"My Very Long Book Title To See If it Fits and to Optimize for Large Titles.",
-    author:"Robert Matthew Rueter von Helsing Schweitzerdreng",
-    language:"Spanish",
-    status:"not read"
-}
-const Card = () => {
+
+const Card = ({ bookInfo }) => {
+    const {title, author, language, status } = bookInfo;
     return(
         <div className="border border-black m-1 flex flex-col justify-around h-60 p-2 text-xs  sm:flex sm:flex-row sm:h-20">
             <div className="sm:w-2/6">
                 <h2 className="text-blue-500">Title:</h2>
-                <p>{example.title}</p>
+                <p>{title}</p>
             </div>
 
             <div className="sm:w-1/6">
                 <p className="text-blue-500"> Author:</p>
-                <p>{example.author} </p>
+                <p>{author} </p>
             </div>
 
             <div className="sm:w-1/6">
                 <p className="text-blue-500">Language:</p>
-                <p>{example.language} </p>
+                <p>{language} </p>
             </div>
 
             <div className="sm:w-1/6">
                 <p className="text-blue-500">Status:</p>
-                <p>{example.status} </p>
+                <p>{status} </p>
             </div>
             <div className="flex justify-end">
                 <div className="m-1">X</div>
