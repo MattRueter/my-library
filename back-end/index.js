946e87b22692a691902aa8b94cb7d9e6ff4c06ba
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-04 14:09:41 -0600

    add morgan to project.

--- back-end/index.js
+++ back-end/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const morgan = require('morgan');
 const port = process.env.PORT
 
+//middleware
+app.use(morgan("tiny"));
+
+//routes
 app.get('/', (req, res) => {
     res.send("Hello My Library")
 });
 
+//server
 app.listen(port, () => {
     console.log(`My Library server listening on port ${port}`)
 });
