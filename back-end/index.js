db9b2c4d01d591ed6d1e9c4394eacf159c57a49f
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-04 15:54:33 -0600

    install passport and auth related dependencies.

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+
+
+dotenv.config();
 const port = process.env.PORT
+const app = express();
+const store = new session.MemoryStore();
+
 
 //middleware
+app.use(cors())
+app.use(express.json())
 app.use(morgan("tiny"));
+
 
 //routes
 app.get('/', (req, res) => {
     res.send("Hello My Library")
 });
+
 
 //server
 app.listen(port, () => {
